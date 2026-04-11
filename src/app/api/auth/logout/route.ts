a791a66c585212dbf20ac9b7a74598d5812e3dc4
Ranel Cator <cator.ranelm@gmail.com>
2026-04-11 20:41:50 +0800

    fix: logout redirect

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const response = NextResponse.redirect(new URL("/login", request.url));
+export async function POST(request: NextRequest) {
+  const response = NextResponse.redirect(new URL("/login", request.url), 303);
 
   response.cookies.set("session", "", {
     httpOnly: true,
